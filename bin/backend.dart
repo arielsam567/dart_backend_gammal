95a9097eccf88be5b9f4f429d46472b5994dd216
arielsam567 <Saidemim112104>
2023-03-22 10:53:29 -0300

    first commit

---
+++ bin/backend.dart
@@ -0,0 +1,9 @@
+//import shelf_io as io;
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+
+void main(List<String> arguments) async {
+  final server = await io.serve((request) => Response(200), '0.0.0.0', 4466);
+
+  print('Serving at ${server.address.address}:${server.port}');
+}
