f261e85e6a630f54f12b8abfcc880a0f5500e100
arielsam567 <Saidemim112104>
2023-03-22 10:56:53 -0300

    AULA 3

--- bin/backend.dart
+++ bin/backend.dart
@@ -3,7 +3,14 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
 void main(List<String> arguments) async {
-  final server = await io.serve((request) => Response(200), '0.0.0.0', 4466);
+  final server = await io.serve(
+    (request) => Response(
+      200,
+      body: 'Hello, world',
+    ),
+    '0.0.0.0',
+    4466,
+  );
 
-  print('Serving at ${server.address.address}:${server.port}');
+  print('Serving at ${server.address.address}:${server.port} - OK');
 }
