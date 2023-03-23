1f4c0d8b1588e63176f4b935bd1819508f021be8
arielsam567 <Saidemim112104>
2023-03-23 06:29:20 -0300

    add resource to users

--- bin/backend.dart
+++ bin/backend.dart
@@ -3,14 +3,24 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as io;
 
 void main(List<String> arguments) async {
-  final server = await io.serve(
-    (request) => Response(
-      200,
-      body: 'Hello, world',
-    ),
-    '0.0.0.0',
-    4466,
-  );
+  Pipeline pipeline = Pipeline().addMiddleware(log());
+
+  final server = await io.serve(pipeline.addHandler(handler), '0.0.0.0', 4466);
 
   print('Serving at ${server.address.address}:${server.port} - OK');
+}
+
+Response handler(Request request) {
+  return Response.ok('Hello, world!');
+}
+
+Middleware log() {
+  return (handler) {
+    return (request) async {
+      print('Request: ${request.method} ${request.requestedUri}');
+      var response = await handler(request);
+      print('Response: ${response.statusCode}');
+      return response;
+    };
+  };
 }
